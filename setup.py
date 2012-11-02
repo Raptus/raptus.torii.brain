cb3a29bdc23db977d04ef9f0239becb610fb476b
Jonas Wagner <jwagner@raptus.com>
2012-11-02 15:05:06 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b1'
 setup(name='raptus.torii.brain',
       version=version,
       description="extends raptus.torii. Allow to store on the fly python-function",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -19,7 +19,7 @@ setup(name='raptus.torii.brain',
       keywords='zope remote prompt zodb storage',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.torii.brain',
+      url='https://github.com/Raptus/raptus.torii.brain',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.torii'],
